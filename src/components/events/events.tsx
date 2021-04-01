1f9bac6f7774dff5930ee5ae5672d54bf8a82c1b
Vika Chepurnaya <v.chepurnayaw@gmail.com>
2021-04-01 17:32:16 +0300

    #26: create routers. update package.json

---
+++ src/components/events/events.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Events: React.FC = () => {
+    return (
+        <div>
+            events component works!
+        </div>
+    );
+};
+
+export default Events;
