5f040ee7b64785d72f590907857bc3057da17c63
Artsiom Gonchar <tema.gon4ar@mail.ru>
2021-04-13 23:21:58 +0300

    Create table and structure for events

--- src/components/events/events.tsx
+++ src/components/events/events.tsx
@@ -1,11 +1,51 @@
-import React from 'react';
+import { FunctionComponent, useCallback, useMemo } from 'react';
+import { GROUPED_COLUMNS } from './columns/GROUPED_COLUMNS';
 
-const Events: React.FC = () => {
-    return (
-        <div>
-            events component works!
-        </div>
-    );
+import dataJson from './data.json';
+import { createStyles, CssBaseline, makeStyles } from '@material-ui/core';
+import Table from '../common/Table/Table';
+import { TableInstance } from 'react-table';
+
+const useStyles = makeStyles( () => {
+  return createStyles({
+    pageWrap : {
+      position: 'relative',
+      paddingLeft: 25,
+      paddingRight: 25,
+      marginTop: 35,
+      marginBottom: 35
+    }
+  })
+})
+
+const Events: FunctionComponent = () => {
+  const classes = useStyles({});
+
+  const columns = useMemo(() => GROUPED_COLUMNS, []);
+  const data = useMemo(() => dataJson.events, []);
+
+  const tempFuncCallback = useCallback(
+    (instance: TableInstance) => {
+      console.log(
+        'Template out text',
+        instance
+      )
+    },
+    []
+  )
+
+  return (
+    <div className={classes.pageWrap}>
+      <CssBaseline />  
+      <Table
+        name={'Events table'}
+        columns={columns}
+        data={data}
+        onAdd={tempFuncCallback}
+        onEdit={tempFuncCallback}
+      />
+    </div>
+  )
 };
 
 export default Events;
