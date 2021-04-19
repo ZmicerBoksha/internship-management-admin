5895e59844f77660bb9ef273f08d0b04524de915
vchepurnaya <52255737+vchepurnaya@users.noreply.github.com>
2021-04-19 15:08:43 +0300

    Candidates (#4)
    * #29: add list of candidates
    
    * #29: update list of candidates. add candidates card
    
    * #26: add progress bar. stylized candidat card
    
    * #29: add progress bar. stylized candidate card
    
    * 29: code refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
-import { FunctionComponent } from 'react';
-import { Switch, Route } from 'react-router-dom';
+import React, { FunctionComponent } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import Dashboard from './components/dashboard/dashboard';
 import Staff from './components/staff/staff';
 import Authorization from './components/authorization/authorization';
 import Navbar from './components/navbar/navbar';
 import Error from './components/path_error/error';
-import Events from './components/Events/Events';
-import EventForm from './components/Events/EventForm/EventForm';
+import Events from './components/events/events';
+import EventForm from './components/events/EventForm/EventForm';
+import CandidatesPage from './components/candidates/candidates_page';
+import CandidateCard from './components/candidates/candidate_card/candidate_card';
+import Axios from 'axios';
+import LRU from 'lru-cache';
+import { configure } from 'axios-hooks';
+
+const axios = Axios.create({
+    baseURL: 'http://localhost:8085/api',
+})
+const cache = new LRU({ max: 10 })
+
+configure({ axios, cache })
 
 const App: FunctionComponent = () => {
     return (
@@ -14,19 +26,21 @@ const App: FunctionComponent = () => {
             <Navbar />
             <Switch>
                 <Route exact path='/' component={Dashboard} />
-                <Route   
-                    path='/events' 
+                <Route
+                    path='/events'
                     component={Events}
                 />
-                <Route   
+                <Route
                     path='/event-form/:eventId?'
                     component={EventForm}
                 />
                 <Route exact path='/staff' component={Staff} />
                 <Route path='/authorization' component={Authorization} />
+                <Route exact path='/candidates' component={CandidatesPage} />
+                <Route path='/candidates/:id' component={CandidateCard} />
                 <Route path='*' component={Error} />
             </Switch>
-        </div>    
+        </div>
     );
 }
 
