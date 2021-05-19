09de7cccaf7a0580ed72e139259734ed2f354637
alibek-begnaev <alibek.begnaev@gmail.com>
2021-05-19 13:58:23 +0500

    sorted code

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,11 @@
 import React, { FunctionComponent, useEffect, useState } from 'react';
 import Navbar from './components/navbar/navbar';
-import Axios from 'axios';
-import LRU from 'lru-cache';
-import { configure } from 'axios-hooks';
+import './service/axios';
 import Routers from './components/routers/routers';
 import { Redirect, Route, Switch, useHistory } from 'react-router-dom';
 import Authorization from './components/authorization/authorization';
 import { config } from 'node:process';
 import Dashboard from './components/dashboard/dashboard';
-
-const axios = Axios.create({
-  baseURL: 'http://localhost:8085/api',
-});
-
-axios.interceptors.request.use(
-  async config => {
-    const token = localStorage.getItem('token');
-
-    if (token) {
-      config.headers = {
-        authorization: `Bearer ${token}`,
-      };
-    }
-    return config;
-  },
-  error => Promise.reject(error),
-);
-
-const cache = new LRU({ max: 10 });
-
-configure({ axios, cache });
 
 const App: React.FC = () => {
   return (
--- src/components/authorization/authorization.tsx
+++ src/components/authorization/authorization.tsx
@@ -63,7 +63,7 @@ const Authorization = () => {
   const classes = useStyles();
   const [{ data: response, loading, error }, executePost] = useAxios<ILoginResponse>(
     {
-      headers: { Authorization: undefined },
+      headers: { Authorization: '' },
       url: `/auth/login`,
       method: 'POST',
     },
