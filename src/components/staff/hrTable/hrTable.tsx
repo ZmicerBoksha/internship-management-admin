9526157319e76b2a957ac7c51eaefbd6df545136
Artsiom Gonchar <69820766+ArteGon@users.noreply.github.com>
2021-05-26 21:59:26 +0300

    Table and events (#29)
    * Update react table and events
    
    * Add work pagination with backend
    
    * Update react table and events
    
    * Add work pagination with backend
    
    * Rebase master and mini refactor table pagination
    
    * Fix mentor`s comments and refactor global filter
    
    * Add env file
    
    * Update routers and add request
    
    * Update routers and add request
    
    * Combine events and candidate, update candidates and requests
    
    * Update api file
    
    * Update react table and events
    
    * Add work pagination with backend
    
    * Update react table and events
    
    * Add work pagination with backend
    
    * Fix mentor`s comments and refactor global filter
    
    * Update routers and add request
    
    * Update routers and add request
    
    * Combine events and candidate, update candidates and requests
    
    * Update api file
    
    * Update work with images
    
    * Update work image
    
    * Update package-lock
    
    * Add roles
    
    * Update create and out info for image
    
    * Fix mini error
    
    * Update events with table and connect with employee
    
    * Fix some buggs
    
    * Add alt text for event image
    
    * Fix bug with show table data
    
    * Add roles and role employee
    
    * Add filters and pagination HR table

--- src/components/staff/hrTable/hrTable.tsx
+++ src/components/staff/hrTable/hrTable.tsx
@@ -1,13 +1,37 @@
-import React, { useCallback, useMemo } from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import Table from '../../common/table/table';
 import useAxios from 'axios-hooks';
-import { PREFIX, HR } from "../../../constants";
+import { PREFIX, HR } from '../../../constants';
 import { SelectColumnFilter } from '../../common/table/filters/selectColumnFilter';
 import { useHistory } from 'react-router';
 import { Button } from '@material-ui/core';
+import { rowsPerPageOptions } from '../../common/table/tablePagination/tablePagination';
+import { countries } from '../../common/countries/countries';
 
 const HrTable: React.FC = () => {
   const [{ data: hrList }, sendRequest] = useAxios(`${PREFIX}employees?search=type==${HR}`);
+
+  const [searchParams, setSearchParams] = useState<string>('');
+  const [dataList, setDataList] = useState<any[]>([]);
+  const [countRows, setCountRows] = useState<number>(0);
+
+  const [page, setPage] = useState<number>(0);
+  const [itemsPerPage, setItemsPerPage] = useState<number>(rowsPerPageOptions[0]);
+  async function getData(page: number, itemsPerPage: number, searchParam?: string) {}
+
+  useEffect(() => {
+    let urlForRequest = `${PREFIX}employees?itemsPerPage=${itemsPerPage}&page=${page}&search=type==${HR}`;
+    if (searchParams.length) urlForRequest += `;${searchParams}`;
+
+    sendRequest({
+      method: 'GET',
+      url: urlForRequest,
+    }).then(response => {
+      setDataList(response.data.content);
+      setCountRows(response.data.totalElements);
+      console.log(response);
+    });
+  }, [page, itemsPerPage, searchParams]);
 
   const history = useHistory();
 
@@ -20,14 +44,14 @@ const HrTable: React.FC = () => {
     history.push(`/staff/${StaffID}`);
   };
 
-  const deleteStaff=(instance: any)=>{
+  const deleteStaff = (instance: any) => {
     const StaffID = instance.rows[0].original.id;
     console.log(instance);
     sendRequest({
       method: 'DELETE',
       url: `${PREFIX}employees/${StaffID}`,
     });
-  }
+  };
 
   const data = useMemo(() => hrList?.content || [], [hrList?.content]);
   const columns = React.useMemo(
@@ -53,30 +77,29 @@ const HrTable: React.FC = () => {
             accessor: 'locationCountry',
             Filter: SelectColumnFilter,
             filter: 'includes',
+            selectValues: countries,
           },
           {
             Header: 'City',
             accessor: 'locationCity',
-            Filter: SelectColumnFilter,
-            filter: 'includes',
           },
           {
             Header: 'Time Zone',
             accessor: 'timezone',
-            Filter: SelectColumnFilter,
-            filter: 'includes',
           },
           {
             Header: 'View',
             accessor: '',
             disableFilters: true,
             Cell: ({ cell }: any) => (
-              <Button style={{ marginTop: '10px' }} variant="contained" color="primary" type="submit"
-
-
-                      onClick={() => {
-                        window.location.href = `/staff/hr/${cell.row.original.id}`;
-                      }}
+              <Button
+                style={{ marginTop: '10px' }}
+                variant="contained"
+                color="primary"
+                type="submit"
+                onClick={() => {
+                  window.location.href = `/staff/hr/${cell.row.original.id}`;
+                }}
               >
                 {'View'}
               </Button>
@@ -109,8 +132,22 @@ const HrTable: React.FC = () => {
     [],
   );
 
-  return <Table name={'HR table'}
-                onDelete={deleteStaff} onAdd={addStaff}  onEdit={editStaff} columns={columns}  data={data} />;
+  return (
+    <Table
+      name={'HR table'}
+      onDelete={deleteStaff}
+      onAdd={addStaff}
+      onEdit={editStaff}
+      columns={columns}
+      data={dataList}
+      setSearchParams={setSearchParams}
+      countRows={countRows}
+      pageNumberForBack={page}
+      setPage={setPage}
+      rowsPerPage={itemsPerPage}
+      setItemsPerPage={setItemsPerPage}
+    />
+  );
 };
 
 export default HrTable;
