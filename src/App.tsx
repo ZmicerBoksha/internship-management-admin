b0a3feaeff0249544516d94bd44db1cee1a8ac70
ZmicerBoksha <zmicerboksha@gmail.com>
2021-05-26 12:46:33 +0300

    remove import

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import React, { FunctionComponent, useEffect, useState } from 'react';
+import React from 'react';
 import Navbar from './components/navbar/navbar';
 import './service/axios';
-import Routers from './components/routers/routers';
 import { Redirect, Route, Switch, useHistory } from 'react-router-dom';
 import Authorization from './components/authorization/authorization';
-import { config } from 'node:process';
-import Dashboard from './components/dashboard/dashboard';
 
 const App: React.FC = () => {
   return (
